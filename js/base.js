b0d5f7d2bc2520092711de4057c86a805ce7e22f
dyf <2537615635@qq.com>
2019-12-24 20:20:54 +0800

    first commit

---
+++ js/base.js
@@ -0,0 +1,15 @@
+//根据QueryString参数名称获取值
+
+function getQueryStringByName(name) {
+
+    var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+
+    if (result == null || result.length < 1) {
+
+        return "";
+
+    }
+
+    return result[1];
+
+}
